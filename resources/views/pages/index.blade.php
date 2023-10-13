5651567eee330d6d9d71ff8ad6bd2a91b545ebf3
NATuan <nat612005@gmail.com>
2023-10-13 14:17:35 +0700

    Add header

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -2,6 +2,59 @@
 
 @section('title', 'Shopping')
 
+@section('styles')
+    <link rel="stylesheet" href="{{ asset('css/index.css') }}">
+@stop
+
 @section('content')
-    <main></main>
+    <main style="height: 3000px">
+        <section class="categories-section">
+            <div class="container p-3" style="background-color: #ffffff">
+                <h3 class="section-title">Categories</h3>
+                <hr>
+                <div class="row g-3">
+                    <div class="col-lg-2 col-md-3 col-sm-4 col-6">
+                        <a href="" class="category-link">
+                            <div class="card">
+                                <img class="card-img-top" src="{{ asset('images/categories/687f3967b7c2fe6a134a2c11894eea4b_tn.png') }}" alt="image">
+                                <div class="card-body">
+                                    <h6 class="card-title text-center">Men Clothes</h6>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-2 col-md-3 col-sm-4 col-6">
+                        <a href="" class="category-link">
+                            <div class="card">
+                                <img class="card-img-top" src="{{ asset('images/categories/31234a27876fb89cd522d7e3db1ba5ca_tn.png') }}" alt="image">
+                                <div class="card-body">
+                                    <h6 class="card-title text-center">Mobile & Gadgets</h6>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-2 col-md-3 col-sm-4 col-6">
+                        <a href="" class="category-link">
+                            <div class="card">
+                                <img class="card-img-top" src="{{ asset('images/categories/978b9e4cb61c611aaaf58664fae133c5_tn.png') }}" alt="image">
+                                <div class="card-body">
+                                    <h6 class="card-title text-center category-name">Consumer Electronics</h6>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-2 col-md-3 col-sm-4 col-6">
+                        <a href="" class="category-link">
+                            <div class="card">
+                                <img class="card-img-top" src="{{ asset('images/categories/c3f3edfaa9f6dafc4825b77d8449999d_tn.png') }}" alt="image">
+                                <div class="card-body">
+                                    <h6 class="card-title text-center category-name">Computer & <Accessories></Accessories></h6>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
 @stop
--- resources/views/partial/header.blade.php
+++ resources/views/partial/header.blade.php
@@ -1,3 +1,51 @@
-<header>
-    asdfasdfasd
+<link rel="stylesheet" href="{{ asset('css/header.css') }}">
+<header class="header">
+    <nav class="navbar navbar-expand-lg">
+        <div class="container">
+            <a class="navbar-brand" href="{{ asset('/') }}">
+                <i class="fa-solid fa-shop"></i>
+                <span>Shopping</span>
+            </a>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="{{ asset('/') }}">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="{{ asset('/products') }}">products</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Categories
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li><hr class="dropdown-divider"></li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </li>
+                </ul>
+                <form class="me-auto" role="search">
+                    <div class="input-group">
+                        <input type="text" class="form-control" placeholder="Search products by name..." aria-label="Search products by name..." aria-describedby="search-button">
+                        <button class="btn btn-warning" type="submit" id="search-button"><i class="fa-solid fa-magnifying-glass"></i></button>
+                    </div>
+                </form>
+                <div>
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link cart-link" aria-current="page" href="{{ asset('/cart') }}"><i class="fa-solid fa-cart-shopping"></i></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="{{ asset('/login') }}">Login</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="{{ asset('/signup') }}">Signup</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </nav>
 </header>
